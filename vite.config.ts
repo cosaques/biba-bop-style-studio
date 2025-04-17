e4f82d35e7b9e31ffbfa6f48acf6f237afc93893
Artem Miniailo <artem.miniailo@gmail.com>
2025-04-17 11:42:23 +0200

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/biba-bop-style-studio/',
   server: {
     host: "::",
     port: 8080,
